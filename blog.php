09af3cef9658beadd59004039be04a94f194a3dc
lyubomyr83 <lyubomyr83@mail.ru>
2019-08-20 18:55:55 +0300

    HTML, CSS разметка

---
+++ blog.php
@@ -0,0 +1,7 @@
+<?php
+require_once "head.php";
+require_once "menu.php";
+?>
+Мой блог
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<div id="menu">
+    <ul>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="contacts.php">Контакты</a></li>
+        <li><a href="blog.php">Мой блог</a></li>
+        <li><a href="http://youtube.com">YouTube</a></li>
+        <li><a href="http://vk.com">ВК</a></li>
+    </ul>
+</div>
